1a4173b5dc3b2bef4d0bdb53421df46306abeeed
Lalit0101 <lalitkp0101@gmail.com>
2024-12-09 22:00:25 +0530

    toon stop

--- scrpit.js
+++ scrpit.js
@@ -3,10 +3,10 @@ let inputDir = { x: 0, y: 0 };
 let lastPaintTime = 0;
 let speed = 5;
 
-const foodSound = new Audio("music/food.mp3");
-const gameOverSound = new Audio("music/gameover.mp3");
-const moveSound = new Audio("music/move.mp3");
-const musicSound = new Audio("music/music.mp3");
+// const foodSound = new Audio("music/food.mp3");
+// const gameOverSound = new Audio("music/gameover.mp3");
+// const moveSound = new Audio("music/move.mp3");
+// const musicSound = new Audio("music/music.mp3");
 
 let snakeArr = [{ x: 13, y: 12 }];
 let food = { x: 5, y: 5 };
